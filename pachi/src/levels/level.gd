3e2987071bc26090f2e6fb6b6cede19cf20b89d3
Axel Magnuson <axelmagn@gmail.com>
2026-01-19 12:17:40 -0800

    more work on basic setup

---
+++ pachi/src/levels/level.gd
@@ -0,0 +1,5 @@
+## A Pachi Level consists of a vertical sequence of sections, which balls fall
+## through until reaching the bottom.  The top is always a ball spawner, and the
+## bottom is always a ball sink.
+class_name Level
+extends Node2D
