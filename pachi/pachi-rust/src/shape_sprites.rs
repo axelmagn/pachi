c82146b0691829377ffed0ffb54347321b67d13d
Axel Magnuson <axelmagn@gmail.com>
2026-04-18 21:57:52 -0700

    more shape sprites

--- pachi/pachi-rust/src/shape_sprites.rs
+++ pachi/pachi-rust/src/shape_sprites.rs
@@ -1,12 +1,16 @@
 use godot::prelude::*;
 
 #[derive(GodotClass)]
-#[class(init, base=Node2D)]
+#[class(init, tool, base=Node2D)]
 struct CircleSprite {
     #[export]
     #[init(val = 32.0)]
     #[var(set=set_radius)]
     radius: f32,
+
+    #[export]
+    #[var(set=set_offset)]
+    offset: Vector2,
 
     #[export]
     #[init(val = Color::WHITE)]
@@ -21,7 +25,8 @@ impl INode2D for CircleSprite {
     fn draw(&mut self) {
         let radius = self.radius;
         let color = self.color;
-        self.base_mut().draw_circle(Vector2::ZERO, radius, color);
+        let offset = self.offset;
+        self.base_mut().draw_circle(offset, radius, color);
     }
 }
 
@@ -34,6 +39,12 @@ impl CircleSprite {
     }
 
     #[func]
+    fn set_offset(&mut self, offset: Vector2) {
+        self.offset = offset;
+        self.base_mut().queue_redraw();
+    }
+
+    #[func]
     fn set_color(&mut self, color: Color) {
         self.color = color;
         self.base_mut().queue_redraw();
@@ -41,11 +52,15 @@ impl CircleSprite {
 }
 
 #[derive(GodotClass)]
-#[class(base=Node2D)]
+#[class(tool, base=Node2D)]
 struct RectSprite {
     #[export]
     #[var(set=set_size)]
     size: Vector2,
+
+    #[export]
+    #[var(set=set_offset)]
+    offset: Vector2,
 
     #[export]
     #[var(set=set_color)]
@@ -59,14 +74,16 @@ impl INode2D for RectSprite {
     fn init(base: Base<Node2D>) -> Self {
         Self {
             size: Vector2::new(64., 32.),
+            offset: Vector2::ZERO,
             color: Color::WHITE,
             base,
         }
     }
 
     fn draw(&mut self) {
-        let rect = Rect2::new(Vector2::ZERO, self.size);
+        let rect = Rect2::new(self.offset, self.size);
         let color = self.color;
+        println!("DRAW RECT");
         self.base_mut().draw_rect(rect, color);
     }
 }
@@ -76,6 +93,12 @@ impl RectSprite {
     #[func]
     fn set_size(&mut self, size: Vector2) {
         self.size = size;
+        self.base_mut().queue_redraw();
+    }
+
+    #[func]
+    fn set_offset(&mut self, offset: Vector2) {
+        self.offset = offset;
         self.base_mut().queue_redraw();
     }
 
