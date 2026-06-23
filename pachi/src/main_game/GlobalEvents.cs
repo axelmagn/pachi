71b76f9331b6ed2a3c05b96020943bdb7ade99d8
Axel Magnuson <axelmagn@gmail.com>
2026-06-22 21:02:39 -0700

    set up fresh project skeleton

---
+++ pachi/src/main_game/GlobalEvents.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+/// Event bus for global events
+public partial class GlobalEvents : Node
+{
+    public static GlobalEvents Instance { get; private set; }
+
+    public override void _EnterTree()
+    {
+        Debug.Assert(Instance == null);
+        Instance = this;
+    }
+}
