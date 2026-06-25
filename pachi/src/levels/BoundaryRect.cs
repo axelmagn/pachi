57ce891c0f4e9b86d093f9ca99ca575225d310c8
Axel Magnuson <axelmagn@gmail.com>
2026-06-24 18:47:13 -0700

    add basic level and hopper

---
+++ pachi/src/levels/BoundaryRect.cs
@@ -0,0 +1,70 @@
+using Godot;
+using System;
+
+[Tool]
+public partial class BoundaryRect : StaticBody2D
+{
+
+    private float _width = 800;
+    private float _height = 400;
+
+    private CollisionShape2D _topBoundary;
+    private CollisionShape2D _bottomBoundary;
+    private CollisionShape2D _leftBoundary;
+    private CollisionShape2D _rightBoundary;
+
+    [Export]
+    public float Width
+    {
+        get => _width;
+        set { _width = value; Rebuild(); }
+    }
+
+    [Export]
+    public float Height
+    {
+        get => _height;
+        set { _height = value; Rebuild(); }
+    }
+
+    [Export]
+    public CollisionShape2D TopBoundary
+    {
+        get => _topBoundary;
+        set { _topBoundary = value; Rebuild(); }
+    }
+
+    [Export]
+    public CollisionShape2D BottomBoundary
+    {
+        get => _bottomBoundary;
+        set { _bottomBoundary = value; Rebuild(); }
+    }
+
+    [Export]
+    public CollisionShape2D LeftBoundary
+    {
+        get => _leftBoundary;
+        set { _leftBoundary = value; Rebuild(); }
+    }
+
+    [Export]
+    public CollisionShape2D RightBoundary
+    {
+        get => _rightBoundary;
+        set { _rightBoundary = value; Rebuild(); }
+    }
+
+    public override void _Ready()
+    {
+        Rebuild();
+    }
+
+    private void Rebuild()
+    {
+        if (_topBoundary != null) _topBoundary.Position = new(0, -_height / 2);
+        if (_bottomBoundary != null) _bottomBoundary.Position = new(0, _height / 2);
+        if (_leftBoundary != null) _leftBoundary.Position = new(-_width / 2, 0);
+        if (_rightBoundary != null) _rightBoundary.Position = new(_width / 2, 0);
+    }
+}
