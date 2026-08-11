bac2f95102900910b45f60b8f6b1d3f142278de5
Axel Magnuson <axelmagn@gmail.com>
2026-08-11 09:22:29 -0700

    add pocket accept sounds

--- pachi/src/pockets/Pocket.cs
+++ pachi/src/pockets/Pocket.cs
@@ -57,6 +57,24 @@ public partial class Pocket : Node2D
     public PocketBallsIndicator OutputsIndicator;
 
     [Export]
+    public AudioStreamPlayer2D AcceptAudioPlayer { get; set; }
+
+    [Export]
+    public AudioStreamPlayer2D RejectAudioPlayer { get; set; }
+
+    [Export]
+    public Array<AudioStream> AcceptAudioStreams { get; set; }
+
+    [Export]
+    public AudioStream RejectAudioStream { get; set; }
+
+    [Export]
+    public bool UsePitchScaleFallback { get; set; } = true;
+
+    [Export]
+    public float SemitonesPerStep { get; set; } = 2.0f;
+
+    [Export]
     public ArmBehavior CatchArmBehavior = ArmBehavior.None;
 
     [Export]
@@ -155,6 +173,9 @@ public partial class Pocket : Node2D
             }
             Debug.Assert(InputBalls.Count == InputBallSlotAvailable.Count);
         }
+
+        AcceptAudioPlayer ??= GetNodeOrNull<AudioStreamPlayer2D>("AcceptAudioPlayer");
+        RejectAudioPlayer ??= GetNodeOrNull<AudioStreamPlayer2D>("RejectAudioPlayer");
     }
 
     private void Rebuild()
@@ -191,19 +212,64 @@ public partial class Pocket : Node2D
 
     }
 
+    private void PlayAcceptSound(int filledSlotIndex)
+    {
+        if (AcceptAudioPlayer == null) return;
+
+        if (AcceptAudioStreams != null && AcceptAudioStreams.Count > 0)
+        {
+            int streamIndex = Mathf.Clamp(filledSlotIndex, 0, AcceptAudioStreams.Count - 1);
+            AcceptAudioPlayer.Stream = AcceptAudioStreams[streamIndex];
+
+            if (filledSlotIndex >= AcceptAudioStreams.Count && UsePitchScaleFallback)
+            {
+                int extraSteps = filledSlotIndex - (AcceptAudioStreams.Count - 1);
+                AcceptAudioPlayer.PitchScale = Mathf.Pow(2.0f, (extraSteps * SemitonesPerStep) / 12.0f);
+            }
+            else
+            {
+                AcceptAudioPlayer.PitchScale = 1.0f;
+            }
+        }
+        else if (AcceptAudioPlayer.Stream != null)
+        {
+            AcceptAudioPlayer.PitchScale = Mathf.Pow(2.0f, (filledSlotIndex * SemitonesPerStep) / 12.0f);
+        }
+
+        AcceptAudioPlayer.Play();
+    }
+
+    private void PlayRejectSound()
+    {
+        if (RejectAudioPlayer == null) return;
+
+        if (RejectAudioStream != null)
+        {
+            RejectAudioPlayer.Stream = RejectAudioStream;
+        }
+        RejectAudioPlayer.PitchScale = 1.0f;
+        RejectAudioPlayer.Play();
+    }
+
     private void OnBallCatch(Ball ball)
     {
+        int filledBefore = 0;
+        foreach (bool available in InputBallSlotAvailable)
+        {
+            if (!available) filledBefore++;
+        }
 
         // accumulate ball
         bool reject = true;
         Debug.Assert(InputBalls.Count == InputBallSlotAvailable.Count);
-            ball.FadeOut(CatchHole.GlobalPosition);
+        ball.FadeOut(CatchHole.GlobalPosition);
         for (int i = 0; i < InputBalls.Count; i++)
         {
             if (InputBalls[i] == ball.Variant && InputBallSlotAvailable[i])
             {
                 InputBallSlotAvailable[i] = false;
                 reject = false;
+                PlayAcceptSound(filledBefore);
                 ball.Connect(Ball.SignalName.FadeOutFinished, Callable.From(ball.QueueFree),
                         (uint)ConnectFlags.OneShot);
                 break;
@@ -213,6 +279,7 @@ public partial class Pocket : Node2D
         // reject ball
         if (reject)
         {
+            PlayRejectSound();
             ball.Connect(Ball.SignalName.FadeOutFinished,
                     Callable.From(() => { ball.FadeIn(RejectHole.GlobalPosition, true); }),
                     (uint)ConnectFlags.OneShot);
